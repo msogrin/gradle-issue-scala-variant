de6f39a6d5e5477e06a95ffa4947fd84bcc30a16
msogrin <msogrin@users.noreply.github.com>
2025-01-02 11:09:50 +0000

    Update build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    scala
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.scala-lang:scala-library:2.13.15")
+    implementation("com.google.guava:guava:33.0.0-jre")
+}
